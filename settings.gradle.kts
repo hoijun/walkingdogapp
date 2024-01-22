71f9684cda355efd74f4c5bcfbcc5041a1cdb410
hoijun <ghlwns10@naver.com>
2024-01-23 01:48:07 +0900

    임시 저장

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://naver.jfrog.io/artifactory/maven/")
+        maven("https://devrepo.kakao.com/nexus/content/groups/public/")
     }
 }
 
