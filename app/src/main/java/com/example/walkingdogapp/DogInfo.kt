8f82ff33dfc956326b020d93c3a1cff723f591dd
hoijun <ghlwns10@naver.com>
2024-03-03 05:50:14 +0900

    산책 화면 이미지 저장 및 산책 좌표 저장
    1. 산책화면 이미지를 그냥 띄울지, 지도를 띄어 좌표를 통해 선을 그릴 지 고민중

--- app/src/main/java/com/example/walkingdogapp/DogInfo.kt
+++ app/src/main/java/com/example/walkingdogapp/DogInfo.kt
@@ -11,17 +11,5 @@ class DogInfo {
     var feature = ""
     var dates = listOf<Walkdate>()
 }
-class Walkdate(day: String, startTime: String, endTime: String, distance: Float, time: Int) {
-    var day = ""
-    var startTime = ""
-    var endTime = ""
-    var distance = 0.0f
-    var time = 0
-    init {
-        this.day = day
-        this.startTime = startTime
-        this.endTime = endTime
-        this.distance = distance
-        this.time = time
-    }
-}
+data class Walkdate(val day: String = "", val startTime: String = "", val endTime: String = "", val distance: Float = 0.0f, val time: Int = 0, val coords: List<WalkLatLng> = listOf<WalkLatLng>())
+data class WalkLatLng(val latititude: Double = 0.0, val longtitude: Double = 0.0)
