e7ed1b77b8bf40c63144e65a0e325853c8f32eb5
hoijun <ghlwns10@naver.com>
2024-04-05 03:28:30 +0900

    재부팅 시 알람
    1. 룸 데이토 베이스에서 알람 리스트를 라이브 데이터로 가져오면 null을 반환. 그래서 대신 리스트를 가져오는 함수를 만들어 재부팅시에도 알람 유지

--- app/src/main/java/com/example/walkingdogapp/alarm/RestartAlarmReceiver.kt
+++ app/src/main/java/com/example/walkingdogapp/alarm/RestartAlarmReceiver.kt
@@ -3,7 +3,6 @@ package com.example.walkingdogapp.alarm
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import com.example.walkingdogapp.userinfo.LocalUserDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -13,20 +12,18 @@ class RestartAlarmReceiver: BroadcastReceiver() {
     private val coroutineScope by lazy { CoroutineScope(Dispatchers.IO) }
     private lateinit var functions: AlarmFunctions
     override fun onReceive(context: Context, intent: Intent) {
-        Log.d("savaepoint", "aaaaa")
         if(intent.action.equals("android.intent.action.BOOT_COMPLETED")) {
             functions = AlarmFunctions(context)
             coroutineScope.launch {
                 val db = LocalUserDatabase.getInstance(context)
-                val list = db!!.alarmDao().getAllAlarms().value?: listOf()
-                val size = list.size
-                Log.d("savaepoint", size.toString())
-                list.let {
-                    for(i in 0 until size) {
-                        val time = list[i].time
-                        val code = list[i].alarm_code
-                        val weeks = list[i].weeks
-                        val alarmOn = list[i].alarmOn
+                val alarmDao = db!!.alarmDao()
+                val alarms = alarmDao.getAlarmsList()
+                alarms.let {
+                    for (i in alarms.indices) {
+                        val time = alarms[i].time
+                        val code = alarms[i].alarm_code
+                        val weeks = alarms[i].weeks
+                        val alarmOn = alarms[i].alarmOn
                         functions.callAlarm(time, code, weeks, alarmOn)
                     }
                 }
