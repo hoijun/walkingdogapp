cb0d39cb4f76b0888a5d41ea49c333827deb8864
hoijun <ghlwns10@naver.com>
2025-02-16 03:50:29 +0900

    앱 시작 부분에 업데이트 체크 기능 추가
    1. 앱 스플래시 액티비티에 업데이트 체크 기능 추가함. 업데이트가 있으면 안내 창이 뜨고 버튼 클릭 시 구글 플레이로 이동.

--- app/src/main/java/com/tulmunchi/walkingdogapp/SplashActivity.kt
+++ app/src/main/java/com/tulmunchi/walkingdogapp/SplashActivity.kt
@@ -1,15 +1,23 @@
 package com.tulmunchi.walkingdogapp
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
+import com.google.firebase.auth.FirebaseAuth
 import com.tulmunchi.walkingdogapp.login.LoginActivity
 import com.tulmunchi.walkingdogapp.utils.FirebaseAnalyticHelper
-import com.google.firebase.auth.FirebaseAuth
+import com.tulmunchi.walkingdogapp.utils.utils.NetworkManager
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -17,6 +25,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlin.system.exitProcess
+
 
 @AndroidEntryPoint
 @SuppressLint("CustomSplashScreen")
@@ -41,14 +50,57 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-
         setContentView(R.layout.activity_splash)
-        this.onBackPressedDispatcher.addCallback(this, callback)
         auth = FirebaseAuth.getInstance()
+    }
 
-        CoroutineScope(Dispatchers.Main).launch {
-            delay(1000)
-            signInFirebase()
+    override fun onResume() {
+        super.onResume()
+        checkUpdate(this)
+        this.onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    private fun checkUpdate(context: Context) {
+        if (!NetworkManager.checkNetworkState(context)) {
+            AlertDialog.Builder(context)
+                .setTitle("네트워크 오류")
+                .setMessage("네트워크 연결을 확인해주세요.\n업데이트 확인을 위해 네트워크 연결이 필요합니다.")
+                .setPositiveButton("재시도") { _, _ ->
+                    checkUpdate(context)
+                }
+                .setNegativeButton("종료") { _, _ ->
+                    finish()
+                }
+                .setCancelable(false)
+                .show()
+            return
+        }
+
+        val appUpdateManager = AppUpdateManagerFactory.create(context)
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+            ) {
+                AlertDialog.Builder(context)
+                    .setTitle("업데이트")
+                    .setMessage("최신 버전으로 업데이트 해주세요!")
+                    .setPositiveButton("확인") { _, _ ->
+                        updateApp(context)
+                    }.setCancelable(false)
+                    .show()
+            } else {
+                CoroutineScope(Dispatchers.Main).launch {
+                    delay(1000)
+                    signInFirebase()
+                }
+            }
+        }.addOnFailureListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                delay(1000)
+                signInFirebase()
+            }
         }
     }
 
@@ -80,6 +132,20 @@ class SplashActivity : AppCompatActivity() {
         } catch (e: Exception) {
             startLogin()
             Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun updateApp(context: Context) {
+        try {
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("market://details?id=${context.packageName}")
+                setPackage("com.android.vending")
+            }
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            val webIntent = Intent(Intent.ACTION_VIEW,
+                Uri.parse("https://play.google.com/store/apps/details?id=${context.packageName}"))
+            startActivity(webIntent)
         }
     }
 
