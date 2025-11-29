46665871122e1802d03e9a2ac8f57aa87061dc5b
hoijun <ghlwns10@naver.com>
2025-11-29 15:21:31 +0900

    release 빌드 방식 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,11 @@ buildscript {
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.5.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.android.test") version "8.1.4" apply false
+    id("com.android.application") version "8.8.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.test") version "8.8.0" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.firebase.crashlytics") version "3.0.2" apply false
-    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
-    id("com.google.dagger.hilt.android") version "2.49" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.28" apply false
+    id("com.google.dagger.hilt.android") version "2.52" apply false
 }
