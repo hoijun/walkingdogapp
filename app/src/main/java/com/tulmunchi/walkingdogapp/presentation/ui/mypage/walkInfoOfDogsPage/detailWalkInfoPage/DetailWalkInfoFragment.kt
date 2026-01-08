d3ddeae87afede5b3fad933c22c7b437f0a530bb
hoijun <wjd3063@gmail.com>
2026-01-08 23:01:16 +0900

    산책 기록 상세 정보 UI 업데이트
    1. 추가된 배변 마커 및 메모 마커를 띄울 수 있도록 업데이트
    2. 시작과 도착 마커를 띄울 수 있도록 업데이트

--- app/src/main/java/com/tulmunchi/walkingdogapp/presentation/ui/mypage/walkInfoOfDogsPage/detailWalkInfoPage/DetailWalkInfoFragment.kt
+++ app/src/main/java/com/tulmunchi/walkingdogapp/presentation/ui/mypage/walkInfoOfDogsPage/detailWalkInfoPage/DetailWalkInfoFragment.kt
@@ -15,6 +15,8 @@ import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
 import com.naver.maps.map.overlay.PathOverlay
 import com.tulmunchi.walkingdogapp.R
 import com.tulmunchi.walkingdogapp.common.GridSpacingItemDecoration
@@ -22,12 +24,11 @@ import com.tulmunchi.walkingdogapp.databinding.FragmentDetailWalkInfoBinding
 import com.tulmunchi.walkingdogapp.domain.model.Dog
 import com.tulmunchi.walkingdogapp.domain.model.WalkRecord
 import com.tulmunchi.walkingdogapp.presentation.core.UiUtils
+import com.tulmunchi.walkingdogapp.presentation.core.dialog.SelectDialog
 import com.tulmunchi.walkingdogapp.presentation.model.CollectionData
 import com.tulmunchi.walkingdogapp.presentation.model.CollectionInfo
-import com.tulmunchi.walkingdogapp.presentation.ui.main.MainActivity
 import com.tulmunchi.walkingdogapp.presentation.ui.main.NavigationManager
 import com.tulmunchi.walkingdogapp.presentation.ui.main.NavigationState
-import com.tulmunchi.walkingdogapp.presentation.ui.mypage.walkInfoOfDogsPage.walkInfoWithCalendarPage.WalkInfoFragment
 import com.tulmunchi.walkingdogapp.presentation.ui.walking.CurrentCollectionItemListAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -37,7 +38,7 @@ class DetailWalkInfoFragment : Fragment(), OnMapReadyCallback { // 수정
     private var _binding: FragmentDetailWalkInfoBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var mynavermap: NaverMap
+    private lateinit var naverMap: NaverMap
     private var walkPath = PathOverlay()
     private lateinit var camera : CameraUpdate
     private var day = listOf<String>()
@@ -86,8 +87,6 @@ class DetailWalkInfoFragment : Fragment(), OnMapReadyCallback { // 수정
         }
 
         val emptyCollections = arrayListOf(CollectionInfo())
-
-
         binding.apply {
             btnBack.setOnClickListener {
                 navigateToWalkInfo()
@@ -119,28 +118,82 @@ class DetailWalkInfoFragment : Fragment(), OnMapReadyCallback { // 수정
     }
 
     override fun onMapReady(map: NaverMap) {
-        this.mynavermap = map
-        mynavermap.uiSettings.setAllGesturesEnabled(false)
-        mynavermap.uiSettings.isZoomControlEnabled = false
+        this.naverMap = map
+        naverMap.uiSettings.setAllGesturesEnabled(false)
+        naverMap.uiSettings.isZoomControlEnabled = false
 
-        binding.zoom.map = mynavermap
+        binding.zoom.map = naverMap
 
         walkPath.width = 15
-        walkPath.color = Color.YELLOW
+        walkPath.color = Color.parseColor("#b67e36")
 
-        val walkCoords = mutableListOf<LatLng>()
-
-        walkRecord?.coords?.forEach { coord ->
-            walkCoords.add(LatLng(coord.latitude, coord.longitude))
-        }
+        val walkCoords = walkRecord?.walkCoordinates?.map {
+            LatLng(it.latitude, it.longitude)
+        } ?: listOf()
 
         if (walkCoords.size > 1) {
-            camera =
-                CameraUpdate.scrollAndZoomTo(walkCoords[walkCoords.size / 2], 16.0) //
-            mynavermap.moveCamera(camera)
+            camera = CameraUpdate.scrollAndZoomTo(walkCoords[walkCoords.size / 2], 16.0) //
+            naverMap.moveCamera(camera)
+
+            addStartMarker(walkCoords.first())
+
+            addEndMarker(walkCoords.last())
+
+            walkRecord?.poopCoordinates?.forEach {
+                addPoopMarker(LatLng(it.latitude, it.longitude))
+            }
+
+            walkRecord?.memoCoordinates?.forEach {
+                addMemoMarker(LatLng(it.value.latitude, it.value.longitude), it.key)
+            }
 
             walkPath.coords = walkCoords
-            walkPath.map = mynavermap
+            walkPath.map = naverMap
+        }
+    }
+
+    private fun addStartMarker(start: LatLng) {
+        val startMarker = Marker()
+        startMarker.position = start
+        startMarker.icon = OverlayImage.fromResource(R.drawable.icon_flag_start)
+        startMarker.width = 150
+        startMarker.height = 150
+        startMarker.map = naverMap
+    }
+
+    private fun addEndMarker(end: LatLng) {
+        val endMarker = Marker()
+        endMarker.position = end
+        endMarker.icon = OverlayImage.fromResource(R.drawable.icon_flag_end)
+        endMarker.width = 150
+        endMarker.height = 150
+        endMarker.map = naverMap
+    }
+
+    private fun addPoopMarker(position: LatLng) {
+        val marker = Marker()
+        marker.position = position
+        marker.icon = OverlayImage.fromResource(R.drawable.icon_poo)
+        marker.width = 150
+        marker.height = 150
+        marker.map = naverMap
+    }
+
+    private fun addMemoMarker(position: LatLng, content: String) {
+        val marker = Marker()
+        marker.position = position
+        marker.icon = OverlayImage.fromResource(R.drawable.icon_note)
+        marker.width = 150
+        marker.height = 150
+        marker.map = naverMap
+        marker.tag = content
+
+        marker.setOnClickListener {
+            // tag에서 메모 내용 가져오기
+            val memoContent = it.tag as? String ?: ""
+            val dialog = SelectDialog.newInstance(title = memoContent)
+            dialog.show(childFragmentManager, "memo")
+            true
         }
     }
 
